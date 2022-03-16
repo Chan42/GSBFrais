d4334eb5bdc4bb8c2c772f2e1eaa19c98e7947f1
Aboubakari Chanfi <aboubakari.chanfi@*gmail.com>
2022-03-16 15:31:09 +0100

    testgsb

---
+++ vues/v_sommaire.php
@@ -0,0 +1,39 @@
+﻿    <!-- Division pour le sommaire -->
+    <div id="menuGauche">
+     <div id="infosUtil">
+    
+        <h2>
+    
+</h2>
+    
+      </div>  
+        <ul id="menuList">
+			<li >
+				  Visiteur :<br>
+				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom'] ?>
+			</li>
+         <li >
+				  Numero de téléphone :<br>
+				<?php echo $_SESSION['tel']  ?>
+			</li>
+         <li >
+				  Numéro d'immatriculation :<br>
+				<?php echo $_SESSION['refvoiture']?>
+			</li>
+         <li >
+				  Type d'energie:  :<br>
+				<?php echo $_SESSION['refenergie']?>
+			</li>
+           <li class="smenu">
+              <a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a>
+           </li>
+           <li class="smenu">
+              <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
+           </li>
+ 	   <li class="smenu">
+              <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
+           </li>
+         </ul>
+        
+    </div>
+    
